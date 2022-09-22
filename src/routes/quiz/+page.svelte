312176688802c35debfac3f9a84cf4b7a558d3eb
Ashok <ashokmarmath8@gmail.com>
2022-09-22 22:12:50 +0530

    Update Done

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -48,12 +48,19 @@
 <div>
     <form>
         <input type="text" bind:value="{question[0].label}">
+        <br/>
         <input type="text" bind:value="{question[0].options[0].label}">
+        <br/>
         <input type="text" bind:value="{question[0].options[1].label}">
+        <br/>
         <input type="text" bind:value="{question[0].options[2].label}">
+        <br/>
         <input type="text" bind:value="{question[0].options[3].label}">
+        <br/>
         <input type="text" bind:value="{question[0].answer}">
+        <br/>
         <input type="text" bind:value="{question[0].explain}">
+        <br/>
         <button type="submit" on:click="{onFormSubmit}">Submit</button>
     </form>
 
@@ -64,7 +71,7 @@
     <p>{question[0].options[3].label}</p>
     <p>{question[0].answer}</p>
     <p>{question[0].explain}</p>
-    
+
 </div>
 
 
