12f829b02c438cc3d2e7491aa1db902e52674605
Ashok <ashokmarmath8@gmail.com>
2022-09-22 12:46:47 +0530

    update quiz

---
+++ src/routes/quiz/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Quiz Panel</title>
+	<meta name="description" content=" Quiz Panel app" />
+</svelte:head>
+
+<div class="content">
+	<h1> Quiz Panel app</h1>
+    
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
