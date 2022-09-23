58170e299f50cbe6ba77efd7031f8e3622d0f2e0
Ashok <ashokmarmath8@gmail.com>
2022-09-23 20:31:04 +0530

    dfghgfd

---
+++ src/routes/todos/[slug]/+page.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+    <title>Edit Quiz</title>
+    <meta name="description" content="Edit Quiz app" />
+
+</svelte:head>
+
+<script>
+    import { page } from '$app/stores';
+
+
+    console.log($page.params)
+
+</script>
+
+
+<div>
+    <h1>{$page.params.slug}</h1>
+    <h2>{$page.params.slug} : Page</h2>
+</div>
