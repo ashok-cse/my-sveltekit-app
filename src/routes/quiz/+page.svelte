41328c06a8c429da6b19a9e3111ebf51a28f577c
Ashok <ashokmarmath8@gmail.com>
2022-09-23 10:40:44 +0530

    table add kr di ha dekho

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -35,7 +35,34 @@
     <input type="text" bind:value={attendee.answer} class="bg-transparent" />
     <input type="text" bind:value={attendee.explanation} class="bg-transparent" />
 	<button type="button" on:click={() => addPerson(attendee)}>add</button>
-    
+
 </div>
 {/each}
 
+
+<table>
+    <thead>
+        <tr>
+            <th>Question</th>
+            <th>Option1</th>
+            <th>Option2</th>
+            <th>Option3</th>
+            <th>Option4</th>
+            <th>Answer</th>
+            <th>Explanation</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each totalAttendees as attendee}
+        <tr>
+            <td>{attendee.question}</td>
+            <td>{attendee.options1}</td>
+            <td>{attendee.options2}</td>
+            <td>{attendee.options3}</td>
+            <td>{attendee.options4}</td>
+            <td>{attendee.answer}</td>
+            <td>{attendee.explanation}</td>
+        </tr>
+        {/each}
+    </tbody>
+</table>
