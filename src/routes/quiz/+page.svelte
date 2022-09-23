d77061c46a1fecce0bcb82bb604c0b0dac4dec84
Ashok <ashokmarmath8@gmail.com>
2022-09-23 12:20:23 +0530

    Update krte krte thak gya bhai

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -21,8 +21,8 @@
 	function addPerson(attendee) {
         if(attendee.question == "" || attendee.options1 == "" || attendee.options2 == "" || attendee.options3 == "" || attendee.options4 == "" || attendee.answer == "" || attendee.explanation == ""){
         }else {
-            checked
-		totalAttendees = [...totalAttendees, attendee];
+        const id = attendee.checked.yes ? attendee.checked.yes : attendee.checked.no;
+        totalAttendees = [...totalAttendees, { ...attendee, id }];
 	}
 }
 	$: attendees = Array(num).fill(defaultAttendee).map((v, i) => ({...v, id: i}));
