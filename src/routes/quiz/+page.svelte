c2df854df8172dac1cf8d47beff34cdc75455282
Ashok <ashokmarmath8@gmail.com>
2022-09-23 10:38:58 +0530

    ye kya hui h suabh

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -7,47 +7,35 @@
 	let num = 0;
 	let totalAttendees = [];
 	let defaultAttendee = {
-		id: 1,
-		question: '',
-		answer: '',
-        explaination: '',
-        options : ''
+		question : "",
+        options1 : "",
+        options2 : "",
+        options3 : "",
+        options4 : "",
+        answer : "",
+        explanation : "",
 	};
+    
 	function addPerson(attendee) {
 		totalAttendees = [...totalAttendees, attendee];
 	}
-	function removePerson(id) {
-        totalAttendees = totalAttendees.filter((attendee) => attendee.id !== id);
-    }
-
-
+	
+	$: attendees = Array(num).fill(defaultAttendee).map((v, i) => ({...v, id: i}));
 </script>
 
+<input bind:value={num} type="number"/>
 
-
+{#each attendees as attendee, i}
 <div>
-	<input type="text" bind:value={attendee.question} class="bg-transparent" placeholder="first" />
-    <input type="text" bind:value={attendee.answer} class="bg-transparent" placeholder="last" />
-    <input type="text" bind:value={attendee.explaination} class="bg-transparent" placeholder="last" />
-    <input type="text" bind:value={attendee.options} class="bg-transparent" placeholder="last" />
-    <button type="button" on:click={() => addPerson(attendee)}>add</button>
+	<input type="text" bind:value={attendee.question} class="bg-transparent" />
+    <input type="text" bind:value={attendee.options1} class="bg-transparent" />
+    <input type="text" bind:value={attendee.options2} class="bg-transparent" />
+    <input type="text" bind:value={attendee.options3} class="bg-transparent" />
+    <input type="text" bind:value={attendee.options4} class="bg-transparent" />
+    <input type="text" bind:value={attendee.answer} class="bg-transparent" />
+    <input type="text" bind:value={attendee.explanation} class="bg-transparent" />
+	<button type="button" on:click={() => addPerson(attendee)}>add</button>
+    
 </div>
+{/each}
 
-
-<div style="display: flex;">
-	<table>
-        <thead>
-            <tr>
-                <th>First</th>
-                <th>Age</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each totalAttendees as attend}
-            <tr>
-                <td>{attend.first}</td>
-                <td>{attend.age}</td>
-            </tr>
-            {/each}
-    </table>
-</div>
