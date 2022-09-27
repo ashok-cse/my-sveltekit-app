39601912e7063d254b315415585908fe736397dc
Ashok <ashokmarmath8@gmail.com>
2022-09-27 22:35:53 +0530

    Faad UI DALA HOON

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -76,12 +76,16 @@
           <p class=" text-white text-xl">{quizdata.question}</p>
         </div>
         {#each options as option}
-        <div class="w-full flex-auto bg-slate-700 rounded pt-4 pr-4 pl-4 pb-4 mb-4">
-          <input type="radio" name="option" id="option" value="{option.value}">
-          <p class=" text-white text-xl">{option.value}</p>
-        </div>
+        <label class="relative block my-2">
+          <span class="sr-only">Search</span>
+          <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+            <input type="radio" name="option" id="option" value="{option.value}">
+          </span>
+          <p class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+            {option.value}
+          </p>
+        </label>
         {/each}
-
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled={submited} on:click={checkAnswer}>
           Submit
         </button>
